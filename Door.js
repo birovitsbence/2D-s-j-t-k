025142de5a24eae47e3868809276a7e19264faa2
lazarboldi <99971876+lazarboldi@users.noreply.github.com>
2025-06-12 14:54:56 +0200

    Játék szépítése, az érthetőbb játékmenet érdekében

--- Door.js
+++ Door.js
@@ -1,93 +1,93 @@
-        class Door {
-            constructor(x, y, w, h) {
-                this.x = x;
-                this.y = y;
-                this.w = w;
-                this.h = h;
-                this.glowIntensity = 0;
-                this.isOpen = false;
-                this.particles = [];
-            }
+class Door {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.glowIntensity = 0;
+        this.isOpen = false;
+        this.particles = [];
+    }
 
-            update(canOpen) {
-                this.isOpen = canOpen;
-                
-                if (this.isOpen) {
-                    this.glowIntensity = 0.5 + Math.sin(millis() * 0.01) * 0.3;
-                    
-                    if (random() < 0.3) {
-                        this.particles.push({
-                            x: this.x + random(this.w),
-                            y: this.y + this.h,
-                            vx: random(-1, 1),
-                            vy: random(-3, -1),
-                            life: 60,
-                            maxLife: 60,
-                            size: random(3, 8)
-                        });
-                    }
-                } else {
-                    this.glowIntensity = 0;
-                }
-                
-                for (let i = this.particles.length - 1; i >= 0; i--) {
-                    const p = this.particles[i];
-                    p.x += p.vx;
-                    p.y += p.vy;
-                    p.life--;
-                    
-                    if (p.life <= 0) {
-                        this.particles.splice(i, 1);
-                    }
-                }
+    update(canOpen) {
+        this.isOpen = canOpen;
+        
+        if (this.isOpen) {
+            this.glowIntensity = 0.5 + Math.sin(millis() * 0.01) * 0.3;
+            
+            if (random() < 0.3) {
+                this.particles.push({
+                    x: this.x + random(this.w),
+                    y: this.y + this.h,
+                    vx: random(-1, 1),
+                    vy: random(-3, -1),
+                    life: 60,
+                    maxLife: 60,
+                    size: random(3, 8)
+                });
             }
-
-            draw() {
-                push();
-                
-                if (this.isOpen && this.glowIntensity > 0) {
-                    fill(6, 182, 212, this.glowIntensity * 100);
-                    rect(this.x - 10, this.y - 10, this.w + 20, this.h + 20, 10);
-                }
-                
-                if (this.isOpen) {
-                    fill(6, 182, 212);
-                } else {
-                    fill(100, 100, 100);
-                }
-                
-                rect(this.x, this.y, this.w, this.h, 5);
-                
-                fill(this.isOpen ? 255 : 150);
-                rect(this.x + 5, this.y + 5, this.w - 10, this.h - 10, 3);
-                
-                fill(255, 215, 0);
-                ellipse(this.x + this.w - 10, this.y + this.h/2, 6, 6);
-                
-                for (const p of this.particles) {
-                    const alpha = map(p.life, 0, p.maxLife, 0, 255);
-                    fill(6, 182, 212, alpha);
-                    ellipse(p.x, p.y, p.size, p.size);
-                }
-                
-                if (!this.isOpen) {
-                    fill(255, 0, 0);
-                    textAlign(CENTER, CENTER);
-                    textSize(12);
-                    text('Gyűjtsd össze\naz érméket!', this.x + this.w/2, this.y - 20);
-                }
-                
-                pop();
-            }
-
-            checkCollision(player) {
-                if (this.isOpen) {
-                    const playerBounds = player.getBounds();
-                    return playerBounds.x < this.x + this.w && 
-                           playerBounds.x + playerBounds.w > this.x &&
-                           playerBounds.y < this.y + this.h && 
-                           playerBounds.y + playerBounds.h > this.y;
-                }
-                return false;
+        } else {
+            this.glowIntensity = 0;
+        }
+        
+        for (let i = this.particles.length - 1; i >= 0; i--) {
+            const p = this.particles[i];
+            p.x += p.vx;
+            p.y += p.vy;
+            p.life--;
+            
+            if (p.life <= 0) {
+                this.particles.splice(i, 1);
             }
         }
+    }
+
+    draw() {
+        push();
+        
+        if (this.isOpen && this.glowIntensity > 0) {
+            fill(6, 182, 212, this.glowIntensity * 100);
+            rect(this.x - 10, this.y - 10, this.w + 20, this.h + 20, 10);
+        }
+        
+        if (this.isOpen) {
+            fill(6, 182, 212);
+        } else {
+            fill(100, 100, 100);
+        }
+        
+        rect(this.x, this.y, this.w, this.h, 5);
+        
+        fill(this.isOpen ? 255 : 150);
+        rect(this.x + 5, this.y + 5, this.w - 10, this.h - 10, 3);
+        
+        fill(255, 215, 0);
+        ellipse(this.x + this.w - 10, this.y + this.h/2, 6, 6);
+        
+        for (const p of this.particles) {
+            const alpha = map(p.life, 0, p.maxLife, 0, 255);
+            fill(6, 182, 212, alpha);
+            ellipse(p.x, p.y, p.size, p.size);
+        }
+        
+        if (!this.isOpen) {
+            fill(255, 0, 0);
+            textAlign(CENTER, CENTER);
+            textSize(12);
+            text('Gyűjtsd össze\naz érméket!', this.x + this.w/2, this.y - 20);
+        }
+        
+        pop();
+    }
+
+    checkCollision(player) {
+        if (this.isOpen) {
+            const playerBounds = player.getBounds();
+            return playerBounds.x < this.x + this.w && 
+                    playerBounds.x + playerBounds.w > this.x &&
+                    playerBounds.y < this.y + this.h && 
+                    playerBounds.y + playerBounds.h > this.y;
+        }
+        return false;
+    }
+}
--- Player.js
+++ Player.js
@@ -1,94 +1,104 @@
-        class Player {
-            constructor(x, y) {
-                this.x = x;
-                this.y = y;
-                this.w = 40;
-                this.h = 60;
-                this.vx = 0;
-                this.vy = 0;
-                this.onGround = false;
-                this.speed = 5;
-                this.jumpPower = 12;
-                this.gravity = 0.5;
-            }
+class Player {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.w = 40;
+        this.h = 60;
+        this.vx = 0;
+        this.vy = 0;
+        this.onGround = false;
+        this.speed = 5;
+        this.jumpPower = 12;
+        this.gravity = 0.5;
+    }
 
-            update(keys, platforms, canvasWidth, canvasHeight) {
-                // Vízszintes mozgás
-                if (keys.has('ArrowLeft')) {
-                    this.vx = -this.speed;
-                } else if (keys.has('ArrowRight')) {
-                    this.vx = this.speed;
-                } else {
-                    this.vx = 0;
-                }
+    update(keys, platforms, canvasWidth, canvasHeight) {
+        // Vízszintes mozgás
+        if (keys.has('ArrowLeft')) {
+            this.vx = -this.speed;
+        } else if (keys.has('ArrowRight')) {
+            this.vx = this.speed;
+        } else {
+            this.vx = 0;
+        }
 
-                // Ugrás
-                if (keys.has('ArrowUp') && this.onGround) {
-                    this.vy = -this.jumpPower;
-                    this.onGround = false;
-                }
+        // Ugrás
+        if (keys.has('ArrowUp') && this.onGround) {
+            this.vy = -this.jumpPower;
+            this.onGround = false;
+        }
 
-                // Gravitáció
-                this.vy += this.gravity;
+        // Gravitáció
+        this.vy += this.gravity;
 
-                // Pozíció frissítése
-                this.x += this.vx;
-                this.y += this.vy;
+        // Pozíció frissítése
+        this.x += this.vx;
+        this.y += this.vy;
 
-                // Platform ütközések
-                this.checkPlatformCollisions(platforms);
+        // Platform ütközések
+        this.checkPlatformCollisions(platforms);
 
-                // Világhatárok
-                if (this.x < 0) this.x = 0;
-                if (this.x + this.w > canvasWidth) this.x = canvasWidth - this.w;
+        // Világhatárok
+        if (this.x < 0) this.x = 0;
+        if (this.x + this.w > canvasWidth) this.x = canvasWidth - this.w;
+        
+        // Leesés ellenőrzése
+        return this.y > canvasHeight;
+    }
+
+    checkPlatformCollisions(platforms) {
+        this.onGround = false;
+        for (const platform of platforms) {
+            if (this.x < platform.x + platform.w && 
+                this.x + this.w > platform.x &&
+                this.y < platform.y + platform.h && 
+                this.y + this.h > platform.y) {
                 
-                // Leesés ellenőrzése
-                return this.y > canvasHeight;
-            }
-
-            checkPlatformCollisions(platforms) {
-                this.onGround = false;
-                for (const platform of platforms) {
-                    if (this.x < platform.x + platform.w && 
-                        this.x + this.w > platform.x &&
-                        this.y < platform.y + platform.h && 
-                        this.y + this.h > platform.y) {
-                        
-                        // Felülről érkezik
-                        if (this.vy > 0 && this.y < platform.y) {
-                            this.y = platform.y - this.h;
-                            this.vy = 0;
-                            this.onGround = true;
-                            
-                            // Bouncy platform
-                            if (platform.type === 'bouncy') {
-                                this.vy = -this.jumpPower * 1.5;
-                                this.onGround = false;
-                            }
-                        }
+                // Felülről érkezik
+                if (this.vy > 0 && this.y < platform.y) {
+                    this.y = platform.y - this.h;
+                    this.vy = 0;
+                    this.onGround = true;
+                    
+                    // Bouncy platform
+                    if (platform.type === 'bouncy') {
+                        this.vy = -this.jumpPower * 1.5;
+                        this.onGround = false;
                     }
                 }
             }
-
-            reset(x, y) {
-                this.x = x;
-                this.y = y;
-                this.vx = 0;
-                this.vy = 0;
-                this.onGround = false;
-            }
-
-            draw() {
-                fill(106, 90, 205);
-                rect(this.x, this.y, this.w, this.h);
-            }
-
-            getBounds() {
-                return {
-                    x: this.x,
-                    y: this.y,
-                    w: this.w,
-                    h: this.h
-                };
-            }
         }
+    }
+
+    reset(x, y) {
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.onGround = false;
+    }
+
+    draw() {
+        fill(106, 90, 205);
+        rect(this.x, this.y, this.w, this.h);
+
+        // Egyszerű arc a játékoson
+                fill(255);
+                ellipse(this.x + 10, this.y + 15, 8, 8); // bal szem
+                ellipse(this.x + 30, this.y + 15, 8, 8); // jobb szem
+
+                fill(255, 100, 100);
+                ellipse(this.x + 20, this.y + 35, 12, 8); // száj
+    }
+
+    
+
+    getBounds() {
+        return {
+            x: this.x,
+            y: this.y,
+            w: this.w,
+            h: this.h
+        };
+    }
+}
